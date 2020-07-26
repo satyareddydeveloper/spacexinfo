d722661a7dff88115d93dde2a89875a8971382a5
satyareddydeveloper <you@example.com>
2020-07-26 11:43:19 +0530

    added header component

---
+++ src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+export class Header extends React.Component{
+    render(){
+        return(
+            <div className = "header">
+           <h1> Welcome to Spacex</h1>
+            </div>
+        );
+    }
+}
